e387c392a0bb0d5ad895fbd62664be625e8636f1
Cyberkn-ight <108725023+Cyberkn-ight@users.noreply.github.com>
2024-11-17 22:35:37 +0100

    Update christmass-tree.c
    why array tho

--- christmass-tree.c
+++ christmass-tree.c
@@ -1,80 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <errno.h>
+#include <unistd.h>
 
-
-
-void sleep(long ms) {
-  struct timespec ts;
-  int res;
-  
-  ts.tv_sec = ms / 1000;
-  ts.tv_nsec = (ms % 1000) * 1000000;
-
-  do {
-    res = nanosleep(&ts, &ts);
-  } while (res == EINTR);
+void msleep(long ms) {
+    usleep(ms * 1000);
 }
-
 
 int main() {
-  // init the prandom generator
-  srandom(time(NULL));
-  
-  // getting the tree size
-  int treeY;
-  printf("type in the size of the tree:\n");
-  scanf("%d", &treeY);
-  int treeX = (2 * treeY) - 1;
+    srand((unsigned int)time(NULL));
 
-  // generate tree
-  int tree[treeY][treeX];
-    // populating
-    // a positive number is the tree, 
-    // -1 means its empty space
-  int s = 1;
-  for (int y = 0; y < treeY; y++) {
-    for (int x = 0; x < treeX; x++) {
-      if (x < s) {
-        tree[y][x] = 1;
-      } else {
-        tree[y][x] = -1;
-      }
-    }
-    s = s + 2;
-  }
-  
-  // displaying tree
-  printf("\x1b[2J");
-  while (1) {
-    printf("\x1b[H");
-    for (int y = 0; y < treeY; y++) {
-      
-      // ((n*2-1) `div` 2) - (x `div` 2)
-      for (int i = 0; i < ((treeY*2-1) / 2) - (((y + 1)*2 - 1) / 2); i++) {
-        printf(" ");
-      }
+    int treeY;
+    printf("Taille du sapin :\n");
+    scanf("%d", &treeY);
+    int treeX = (2 * treeY) - 1;
 
-      for (int x = 0; x < treeX; x++) {
-        if (tree[y][x] != -1) {
-
-          if (x == 0 && y == 0) {
-            printf("\x1b[38;2;212;175;55m*\x1b[0m");
-
-          } else if (60 < random() % 100) { // about 40% chance for a light
-            printf("\x1b[38;2;%ld;%ld;%ldm*\x1b[0m", random() % 255, random() % 255, random() % 255);
-
-          } else { // dark green for unlit parts of tree
-            printf("\x1b[38;2;0;100;0m*\x1b[0m");
-
-          }
+    while (1) {
+        printf("\033[2J\033[H");  // Efface l'écran et déplace le curseur
+        for (int y = 0, s = 1; y < treeY; y++, s += 2) {
+            int offset = (treeX - s) / 2;
+            for (int i = 0; i < offset; i++) printf(" ");
+            for (int x = 0; x < s; x++) {
+                if (y == 0 && x == 0) {
+                    printf("\033[38;2;212;175;55m*\033[0m");
+                } else if (rand() % 100 < 40) {
+                    printf("\033[38;2;%d;%d;%dm*\033[0m", rand() % 255, rand() % 255, rand() % 255);
+                } else {
+                    printf("\033[38;2;0;100;0m*\033[0m");
+                }
+            }
+            printf("\n");
         }
-      }
-      printf("\n");
+        msleep(300);
     }
-    sleep(300);
-  }
-  return 0;
+    return 0;
 }
-
